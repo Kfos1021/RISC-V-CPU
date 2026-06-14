3ee4991c21e73352d47441dafd061c1f9927ef00
Kyle <kylefossen21@gmail.com>
2026-06-14 15:35:04 -0400

    Ignore Verilator build output

--- obj_dir/tb_alu.d
+++
@@ -1,6 +0,0 @@
-tb_alu.o: ../sim/tb_alu.cpp Valu.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.048/share/verilator/include/verilated_funcs.h
